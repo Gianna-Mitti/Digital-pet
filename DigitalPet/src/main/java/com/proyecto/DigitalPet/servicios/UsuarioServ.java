4e8204f4f41170f590f326169757aaae76078e5a
camilapizzera <camila_pizzera@hotmail.com>
2022-04-27 21:00:08 -0300

    4to commit, UsuarioServ.

--- DigitalPet/src/main/java/com/proyecto/DigitalPet/servicios/UsuarioServ.java
+++ DigitalPet/src/main/java/com/proyecto/DigitalPet/servicios/UsuarioServ.java
@@ -1,13 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.proyecto.DigitalPet.servicios;
 
 import com.proyecto.DigitalPet.entidades.Usuario;
 import com.proyecto.DigitalPet.errores.ErrorServicio;
 import com.proyecto.DigitalPet.repositorios.UsuarioRepo;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -19,23 +16,88 @@ import org.springframework.transaction.annotation.Transactional;
  */
 @Service
 public class UsuarioServ {
-    
+
     @Autowired
     private UsuarioRepo usuarioRepo;
-    
-     @Transactional
-    public Usuario registrar(String nombre, String apellido, String mail, Long tel, String clave, String clave2) throws ErrorServicio {
-        validator(nombre, apellido, mail, tel, clave, clave2, null);
-        Usuario usuario=new Usuario();
+
+    @Transactional
+    public Usuario registrar(String nombre, String apellido, String mail, Long tel, String clave) throws ErrorServicio {
+        validator(nombre, apellido, mail, clave);
+        Usuario usuario = new Usuario();
         usuario.setNombre(nombre);
         usuario.setApellido(apellido);
+        usuario.setTel(tel);
         usuario.setMail(mail);
         String encriptada = new BCryptPasswordEncoder().encode(clave);
         usuario.setClave(encriptada);
         return usuarioRepo.save(usuario);
     }
-    
-    public void validator (String nombre, String apellido, String mail, Long tel, String clave, String clave2, String validar) throws ErrorServicio {
+
+    @Transactional
+    public Usuario modificar(String id, String nombre, String apellido, String mail, Long tel, String clave) throws ErrorServicio {
+        Optional<Usuario> op = usuarioRepo.findById(id);
+
+        if (op.isPresent()) {
+            Usuario usuario = op.get();
+            if (clave.equals(usuario.getClave())) {
+                validator(nombre, apellido, mail, clave);
+
+                usuario.setNombre(nombre);
+                usuario.setApellido(apellido);
+                usuario.setTel(tel);
+                usuario.setMail(mail);
+                String encriptada = new BCryptPasswordEncoder().encode(clave);
+                usuario.setClave(encriptada);
+
+                return usuarioRepo.save(usuario);
+            }else{
+                throw new ErrorServicio("La clave es incorrecta");
+            }
+        } else {
+            throw new ErrorServicio("No se ha modificado correctamente");
+        }
+    }
+    @Transactional
+    public Usuario modificarClave (String id, String claveNueva, String claveAnterior) throws ErrorServicio {
+        Optional<Usuario> op = usuarioRepo.findById(id);
+         if (op.isPresent()) {
+            Usuario usuario = op.get();
+             validarClave(claveAnterior);
+            if (claveAnterior.equals(usuario.getClave())) {
+                validarClave(claveNueva);
+                String encriptada = new BCryptPasswordEncoder().encode(claveNueva);
+                usuario.setClave(encriptada);
+                return usuarioRepo.save(usuario);
+            }else{
+                throw new ErrorServicio("La clave es incorrecta");
+            }
+         }else{
+             throw new ErrorServicio("No se ha modificado correctamente");
+         }
+    }
+    @Transactional
+    public void eliminar(String id) {
+        usuarioRepo.deleteById(id);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Usuario> findAll() {
+        return usuarioRepo.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Usuario getOne(String id) {
+        return usuarioRepo.getById(id);
+
+    }
+
+    public void validarClave(String clave) throws ErrorServicio {
+if (clave == null || clave.trim().isEmpty() || clave.length() <= 6) {
+            throw new ErrorServicio("La clave del usuario no puede estar vacia, y debe tener mas de 6 digitos.");
+        }
+}
+
+    public void validator(String nombre, String apellido, String mail, String clave) throws ErrorServicio {
 
         if (nombre == null || nombre.trim().isEmpty()) {
             throw new ErrorServicio("El nombre del usuario no puede estar vacio.");
@@ -49,20 +111,13 @@ public class UsuarioServ {
             throw new ErrorServicio("El mail del usuario no puede estar vacio.");
         }
 
-        if (validar == null) {
-            if (usuarioRepo.buscarxMail(mail) != null) {
-                throw new ErrorServicio("Ya existe un usuario creado con ese email.");
-            }
+        if (usuarioRepo.buscarxMail(mail) != null) {
+            throw new ErrorServicio("El mail ya esta registrado en la base de datos.");
         }
 
-        if (clave== null || clave.trim().isEmpty() || clave.length() <= 6) {
+        if (clave == null || clave.trim().isEmpty() || clave.length() <= 6) {
             throw new ErrorServicio("La clave del usuario no puede estar vacia, y debe tener mas de 6 digitos.");
         }
-
-        if (!clave.equals(clave2)) {
-            throw new ErrorServicio("Las contraseñas deben ser iguales.");
-        }
-
     }
-    
+
 }
