bd4f188a8b5c9339f58f0246299baae972058918
tatianaacabrera <cabreraprovinzanotatianaaylen@gmail.com>
2022-05-05 19:20:48 -0300

    Modificación javaScript

--- DigitalPet/src/main/resources/static/static.js/form-mascota.js
+++ DigitalPet/src/main/resources/static/static.js/form-mascota.js
@@ -3,7 +3,7 @@ const inputs = document.querySelectorAll('#form input');
 
 const expressions = {
     name: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
-	//fechaNac: /^[0-9]{2}[/][0-9]{2}[/][0-9]{4}$/, /* validacion dd/mm/aaaa */
+	fechaNac: /^[0-9]{2}[/][0-9]{2}[/][0-9]{4}$/, /* validacion dd/mm/aaaa */
 	//sexo:/^(macho|hembra)*$/,
 	//especie: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 	//vacAplicadas: /^.{4,12}$/,
@@ -11,7 +11,7 @@ const expressions = {
 
 const fields = {
 	name: false,
-	// fechaNac: false,
+	fechaNac: false,
 	// sexo: false,
 	// especie: false,
 	// vacAplicadas: false,
@@ -31,14 +31,14 @@ const fields = {
 //   return false;
 // }
 
-// let ano = document.getElementById("ano").value;
-// let mes = document.getElementById("mes").value;
-// let dia = document.getElementById("dia").value;
+let ano = document.getElementById("ano").value;
+let mes = document.getElementById("mes").value;
+let dia = document.getElementById("dia").value;
 
-// fechaNac = new Date(ano, mes, dia);
-// if(!isNaN(fechaNac)){
-// 	return false;
-// }
+fechaNac = new Date(ano, mes, dia);
+if(!isNaN(fechaNac)){
+ 	return false;
+}
 
 // indice = document.getElementById("especie").selectedIndex;
 // if( indice == null || indice == 0 ) {
@@ -50,9 +50,9 @@ const validateForm = (e) => {
 		case "name":
 			validateField(expressions.name, e.target, 'name');
 		break;
-		// case "fechaNac":
-		// 	validateField(expressions.fechaNac, e.target, 'fechaNac');
-		// break;
+		case "fechaNac":
+			validateField(expressions.fechaNac, e.target, 'fechaNac');
+		break;
 		// case "sexo":
 		// 	validateField(expressions.sexo, e.target, 'sexo');
 		// break;
@@ -82,38 +82,8 @@ const validateField = (expression, input, field) => {
 		fields[field] = false;
 	}
 }
-// const validatePasswordi = () => {
-// 	const inputPassword = document.getElementById('password');
-// 	const inputPasswordi = document.getElementById('passwordi');
 
-// 	if(inputPassword.value !== inputPasswordi.value){
-// 		document.getElementById(`group-passwordi`).classList.add('form-group-wrong');
-// 		document.getElementById(`group-passwordi`).classList.remove('form-group-right');
-// 		document.querySelector(`#group-passwordi i`).classList.add('fa-times-circle');
-// 		document.querySelector(`#group-passwordi i`).classList.remove('fa-check-circle');
-// 		document.querySelector(`#group-passwordi .form-input-error`).classList.add('form-input-error-active');
-// 		fields['password'] = false;
-// 	} else {
-// 		document.getElementById(`group-passwordi`).classList.remove('form-group-wrong');
-// 		document.getElementById(`group-passwordi`).classList.add('form-group-right');
-// 		document.querySelector(`#group-passwordi i`).classList.remove('fa-times-circle');
-// 		document.querySelector(`#group-passwordi i`).classList.add('fa-check-circle');
-// 		document.querySelector(`#group-passwordi .form-input-error`).classList.remove('form-input-error-active');
-// 		fields['password'] = true;
-// 	}
-// }
 inputs.forEach((input) => {
 	input.addEventListener('keyup', validateForm);
 	input.addEventListener('blur', validateForm);
 });
-
-// form.addEventListener('submit', (e) => {
-// 	const terms = document.getElementById('terms');
-// 	if(!(fields.name && fields.surname && fields.mail && fields.password && terms.checked) ){
-// 		e.preventDefault();
-// 		document.getElementById('form-message').classList.add('form-message-active');
-// 		setTimeout(() => {
-// 			document.getElementById('form-message').classList.remove('form-message-active');
-// 		}, 5000);
-// 	} 
-// });
